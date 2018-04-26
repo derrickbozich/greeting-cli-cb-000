1b49f79b06d534bf45a585984e65c2bf7bc3de91
derrick b <dbozich1@gmail.com>
2018-04-26 00:17:21 +0000

    automatically backed up by learn

--- spec/02_cli_spec.rb
+++ spec/02_cli_spec.rb
@@ -10,30 +10,6 @@ describe './bin/greet executing a CLI Application' do
     run_file("./bin/greet")
   end
 
-  it 'uses #gets.strip to capture the user input and set it equal to a variable called name' do
-    allow($stdout).to receive(:puts)
-
-    expect(self).to receive(:gets).and_return("Sally")
-    name = get_variable_from_file("./bin/greet", "name")
-
-    expect(name).to eq("Sally")
+  
   end
 
-  it "calls on the #greeting method with an argument of the user's name" do
-    allow($stdout).to receive(:puts)
-    allow(self).to receive(:gets).and_return("Sally")
-
-    expect(self).to receive(:greeting).with("Sally"), "Make sure the bin/greet file has code that calls the #greeting method with an argument of the user's name."
-
-    run_file("./bin/greet")
-  end
-
-  it "calls on the #greeting method with an argument of the user's name and returns the new greeting, interpolating the user's name" do
-    allow($stdout).to receive(:puts)
-    allow(self).to receive(:gets).and_return("Don")
-
-    output = capture_puts{ run_file("./bin/greet") }
-
-    expect(output).to include("Hello Sally. It's nice to meet you.")
-  end
-end
